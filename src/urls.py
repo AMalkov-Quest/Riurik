addd4ca64127768db5bc0f8b85047b4d5c009ff6
Anton Timiskov <anton.timiskov@quest.com>
2011-04-04 14:35:32 +0400

    enumerate suites

--- src/urls.py
+++ src/urls.py
@@ -16,6 +16,33 @@ from django.views.static import serve
 import os
 import settings
 
+def enumerate_suites(request):
+	"""
+		Return a list of suite names.
+		Arguments:
+			context	(optional)	- filter suites containing supplied context name
+			json 	(optional)	- return result in JSON format
+	"""
+	context = request.REQUEST.get('context', None)
+	json = request.REQUEST.get('json', False)
+	from django.http import HttpResponse
+	from context import get as context_get
+	suites = []
+	_root = settings.STATIC_TESTS_ROOT
+	contextini = '.context.ini'
+	for dirpath, dirnames, filenames in os.walk(_root, followlinks=True):
+		if not ( contextini in filenames ): continue
+		if context:
+			contextfile = os.path.join(dirpath, contextini)
+			ctx = context_get(contextfile)
+			ctx_sections = ctx.sections()
+			if not context in ctx_sections: continue
+		suites += [ dirpath.replace(_root, '').lstrip('/') ]
+	if json:
+		import simplejson
+		return HttpResponse(simplejson.dumps(suites))
+	return HttpResponse(str(suites).replace('[','').replace(']','').rstrip(','))
+
 urlpatterns = patterns('',
 	(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/img/favicon.gif'}),
 	(r'^websocket$', 'views.handler' ),
@@ -33,6 +60,7 @@ urlpatterns = patterns('',
 	(r'^actions/test/control/$', 'views.getControl'),
 	(r'^logger/records/recv/$', 'views.recvLogRecords'),
 	(r'^actions/remove/$', 'views.removeObject'),
+	(r'^actions/suite/enumerate/$', enumerate_suites),
 )
 
 urlpatterns += patterns('',
