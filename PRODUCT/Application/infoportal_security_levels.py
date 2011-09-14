ab65eacf282762e4e2d623a8463b2d5907f36ec0
Andrew Malkov <andrey.malkov@quest.com>
2011-09-14 12:01:41 +0400

    del unused folder

--- PRODUCT/Application/infoportal_security_levels.py
+++
@@ -1,5 +0,0 @@
-#coding utf-8
-
-# PUT THIS TO YOUR infoportal_security_levels.py
-
-UnSecurePages += ["/tests/"]
--- PRODUCT/Application/tests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from django.views.static import serve
-import os
-
-urlpatterns = patterns('Application.tests.views',
-	(r'^$', 'index'),
-)
