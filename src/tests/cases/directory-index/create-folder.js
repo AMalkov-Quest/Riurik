0fcb0f52c1d1224d84f4818144eb9962cb14a885
Andrew Malkov <andrey.malkov@quest.com>
2011-12-12 13:33:20 +0400

    fix fragile test

--- src/tests/cases/directory-index/create-folder.js
+++ src/tests/cases/directory-index/create-folder.js
@@ -7,40 +7,33 @@ QUnit.setup(function() {
 
 asyncTest('check created', function() {
   $.when( frame.go(contexter.URL(context, context.root)) ).then(function(_$) {
+    _$('a#new-suite').click();
+    equal(_$('#create-dir-index-dialog').is(":visible"), true, 'dialog is visible');
+    equal(_$('.ui-dialog-title').text(), _$('a#new-suite').text(), 'dialog has right title');
     
-    $.when( _$('a#new-suite').click() ).then(function() {
+    _$('#object-name').val(context.folder_name);
+    _$('#create-folder-btn').click();
+    
+    $.when( frame.load() ).then(function(_$) {
+      equal(_$('li#'+ context.folder_name + '.folder').length, 1, 'new folder has been created');
+      var folder_link = _$('li#'+ context.folder_name + ' > a');
+      ok(folder_link.is(":visible"), 'link to the folder is visible');
+      equal(folder_link.attr('href'), context.folder_name + '/', 'link to the folder has right href');
       
-      equal(_$('#create-dir-index-dialog').is(":visible"), true, 'dialog is visible');
-      equal(_$('.ui-dialog-title').text(), _$('a#new-suite').text(), 'dialog has right title');
-      
-      _$('#object-name').val(context.folder_name);
-      _$('#create-folder-btn').click();
-      
-      $.when( frame.load() ).then(function(_$) {
-        equal(_$('li#'+ context.folder_name + '.folder').length, 1, 'new folder has been created');
-        var folder_link = _$('li#'+ context.folder_name + ' > a');
-        ok(folder_link.is(":visible"), 'link to the folder is visible');
-        equal(folder_link.attr('href'), context.folder_name + '/', 'link to the folder has right href');
-        
-        start();
-      });
+      start();
     });
   });
 });
 
 asyncTest('error reporting', function() {
   $.when( frame.go(contexter.URL(context, context.root)) ).then(function(_$) {
-    $.when( _$('a#new-suite').click() ).then(function() {
-      
-      _$('#object-name').val(context.folder_name);
-      _$('#create-folder-btn').click();
-      
-      $.wait( function() { return _$('#operationResult').is(":visible") }, 1000 ).then(function() {
-        QUnit.substring( _$('#operationResult').text(), 'File exists', 'dialog with error is visible');
-        _$('.ui-button-text', _$('#operationResult').parent()).first().click();
-        
-        start();
-      });
+    _$('a#new-suite').click()
+    _$('#object-name').val(context.folder_name);
+    _$('#create-folder-btn').click();
+    
+    $.wait( function() { return _$('#operationResult').is(":visible") }, 1000 ).then(function() {
+      QUnit.substring( _$('#operationResult').text(), 'File exists', 'dialog with error is visible');
+      start();
     });
   });
 });
