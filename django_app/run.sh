bd371a2b6cc3f7f237c0eeed95a20e6d3b7b90e4
Andrew Malkov <andrey.malkov@quest.com>
2011-12-16 16:16:22 +0400

    remove unused folder

--- django_app/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# coding: utf-8
-
-python manage.py runserver 0.0.0.0:8002
--- django_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
-def hello(request):
-	#return HttpResponse('Hello world!')
-	return render_to_response('default.html')
