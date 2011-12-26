a0d56197114253f618aa1e7d1982d20eea77bb4e
Andrew Malkov <andrey.malkov@quest.com>
2011-12-26 10:50:38 +0400

    editing a test without context proposes creating context

---
+++ src/tests/cases/test-editor/new-suite.js
@@ -0,0 +1,22 @@
+module('edit test without context');
+
+QUnit.setup(function() {
+  with(context) {
+    context.suite_name = 'new-suite';
+    context.suite_path = root.concat('/', suite_name);
+    context.test_name = 'new-test.js';
+    context.test_path = suite_path.concat('/', test_name);
+    
+    create_folder(suite_name, root);
+    create_test(test_name, context.suite_path);
+  }
+});
+
+asyncTest('should propose creating context', function() {
+  $.when(frame.go(context.test_path + '?editor')).then(function(_$) {
+    equal( _$('#context-preview-ctrl').text().trim(), 'Create context' );
+    equal( _$('#context-preview-ctrl').attr('title').trim(), 'To execute the test you have to define cotext' );
+    
+    start();
+  });
+});
