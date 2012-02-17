b4b1930dd372d499e230dc676db6eb9a0c31aba0
Andrew Malkov <andrey.malkov@gmail.com>
2012-02-17 05:47:48 +0400

    CoffeeScript and platform: errors handling

--- src/coffeescript.py
+++ src/coffeescript.py
@@ -11,18 +11,18 @@ else:
 ext = '.coffee'
 
 def coffee2js(path):
-    file_name = path.rsplit('/', 1)[1]
+    file_name = os.path.basename(path)
     return (file_name, '.%s' % file_name.replace(ext, '.js'))
 
 def execute(source, full_path):
     try:
         args = shlex.split("%s -c -b -s -p" % COFFEESCRIPT_EXECUTABLE)
         p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        out, errors = p.communicate(source.encode("utf-8"))
+        return p.communicate(source.encode("utf-8"))
     except Exception, e:
         raise Exception("Can't execute CoffeeScript compiler: %s" % COFFEESCRIPT_EXECUTABLE)
         
-def save(full_path):
+def save(full_path, path, out):
     coffee_name, js_name = coffee2js(full_path)
     full_path = full_path.replace(coffee_name, js_name)
     if os.path.exists(full_path):
@@ -38,5 +38,5 @@ def compile(source, path, full_path):
     if not source:
         source = dir_index_tools.gettest(full_path)
     out, errors = execute(source, full_path)
-    return save(full_path)
+    return save(full_path, path, out)
         
