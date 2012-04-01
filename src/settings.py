8864f28c21c99dcc210fd095438a3943edf44838
Andrew Malkov <andrey.malkov@gmail.com>
2012-04-01 23:07:22 +0400

    fixed Error importing template source loader (new Dgango)

--- src/settings.py
+++ src/settings.py
@@ -72,8 +72,8 @@ SECRET_KEY = '(nf!nzz3a&uxzbdb)9*hor38)*a4&m)szzbn$&8wy*&h7t*v_-'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 #SESSION_ENGINE = 'django.contrib.sessions.backends.file'
