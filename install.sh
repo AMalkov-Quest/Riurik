6bd1255171405aac5e4b7bb8da70ea6e5cdfc80f
Andrew Malkov <Andrey.Malkov@quest.com>
2012-05-02 09:50:35 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,21 +6,21 @@
 # sudo apt-get install python-pip
 
 # install Django
-pip install Django
+#pip install Django
 
 # install nodejs
-apt-get install python-software-properties
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install nodejs npm
+#apt-get install python-software-properties
+#add-apt-repository ppa:chris-lea/node.js
+#apt-get update
+#apt-get install nodejs npm
 
 # install CoffeeScript
-npm install -g coffee-script
+#npm install -g coffee-script
  
 # install Riurik
 git clone git@github.com:andrew-malkov/Riurik.git
 cd ~/Riurik/src
 git checkout quick_tests_support
-sh autoconf.sh hunterbranch1 ~/SASPHunter
+#sh autoconf.sh hunterbranch1 ~/SASPHunter
 
 
