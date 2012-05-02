1e558ab9f993125f5cf31790b7cc8aba76c17563
Andrew Malkov <Andrey.Malkov@quest.com>
2012-05-02 09:49:54 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,25 @@
 
 # A word about this shell script:
 #
-# ...
+# the pip package should be installed previously
+# sudo apt-get install python-pip
+
+# install Django
+pip install Django
+
+# install nodejs
+apt-get install python-software-properties
+add-apt-repository ppa:chris-lea/node.js
+apt-get update
+apt-get install nodejs npm
+
+# install CoffeeScript
+npm install -g coffee-script
+ 
+# install Riurik
+git clone git@github.com:andrew-malkov/Riurik.git
+cd ~/Riurik/src
+git checkout quick_tests_support
+sh autoconf.sh hunterbranch1 ~/SASPHunter
+
 
