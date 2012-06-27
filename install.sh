5915d0bc077b70c1c6320b997754ccda38d716c9
Andrew Malkov <andrey.malkov@quest.com>
2012-06-27 18:17:42 +0400

    support search in tests

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-# A word about this shell script:
-#
+# How to use this script
+#curl https://raw.github.com/andrew-malkov/Riurik/master/install.sh | BRANCH=excessive_versions_cleanup_build sh
+
 # the pip package should be installed previously
-sudo apt-get install -y python-pip
+##sudo apt-get install -y python-pip
 
 # install Django
-sudo pip install Django
+##sudo pip install Django
 
 # install nodejs
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs npm
+##sudo apt-get install -y python-software-properties
+##sudo add-apt-repository ppa:chris-lea/node.js
+##sudo apt-get update
+##sudo apt-get install -y nodejs npm
 
 # install CoffeeScript
-sudo npm install -g coffee-script
+##sudo npm install -g coffee-script
 
 # checkout production code
 git clone git@github.com:andrew-malkov/SASP.git SASPHunter
