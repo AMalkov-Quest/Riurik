b1852852d2a3d48ec65204d2b9cc6a6e35cf2a26
Andrew Malkov <andrey.malkov@quest.com>
2012-06-27 19:03:00 +0400

    support search in tests

--- install.sh
+++ install.sh
@@ -4,29 +4,29 @@
 #curl https://raw.github.com/andrew-malkov/Riurik/master/install.sh | BRANCH=excessive_versions_cleanup_build sh
 
 # the pip package should be installed previously
-##sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip
 
 # install Django
-##sudo pip install Django
+#sudo pip install Django
 
 # install nodejs
-##sudo apt-get install -y python-software-properties
-##sudo add-apt-repository ppa:chris-lea/node.js
-##sudo apt-get update
-##sudo apt-get install -y nodejs npm
+#sudo apt-get install -y python-software-properties
+#sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-get update
+#sudo apt-get install -y nodejs npm
 
 # install CoffeeScript
-##sudo npm install -g coffee-script
+#sudo npm install -g coffee-script
 
 # checkout production code
-##git clone git@github.com:andrew-malkov/SASP.git SASPHunter
-##cd SASPHunter
-##git checkout $BRANCH
+git clone git@github.com:andrew-malkov/SASP.git SASPHunter
+cd SASPHunter
+git checkout $BRANCH
 
-##cd ..
+cd ..
  
 # install Riurik
-##git clone git@github.com:andrew-malkov/Riurik.git
+git clone git@github.com:andrew-malkov/Riurik.git
 cd Riurik/src
 git checkout quick_tests_support
 sh autoconf.sh info-portal-tests ../../SASPHunter
