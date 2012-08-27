eb897acca3b447f523f998e064b047c7090c5bb6
Ubuntu <ubuntu@domU-12-31-39-00-E6-AC.compute-1.internal>
2012-08-27 14:50:32 +0000

    working with repo by input keys

--- src/auth/gitssh.py
+++ src/auth/gitssh.py
@@ -1,16 +1,18 @@
-import subprocess, tempfile, os
+import subprocess, tempfile, os, shlex
 
-git_ssh_cmd = os.path.abspath( os.path.join( os.path.dirname( os.path.join( __file__ ) ), 'gitssh.sh' ) )
+git_ssh_cmd = os.path.abspath( os.path.join( os.path.dirname( os.path.join( __file__ ) ), 'git-ssh.sh' ) )
 
 class GitSSH(object):
-	def __init__(self, privateKey, publicKey):
-		hPrivate, self.privateFile = tempfile.mkstemp()
+	def __init__(self, repoPath, privateKey, publicKey):
+		h, self.privateFile = tempfile.mkstemp()
+		hPrivate = open(self.privateFile, 'w')
 		hPrivate.write(privateKey)
 		hPrivate.close()
 		self.publicFile = self.privateFile + '.pub'
 		hPublic = open(self.publicFile, 'w')
 		hPublic.write(publicKey)
 		hPublic.close()
+		self.repository = repoPath
 
 	def __enter__(self):
 		return self.popen
@@ -18,6 +20,7 @@ class GitSSH(object):
 	def __exit__(self, exceptionType, value, traceback):
 		os.remove(self.privateFile)
 		os.remove(self.publicFile)
+		print exceptionType, value, traceback
 		return True # The exception was not handled
 
 	def popen(self, cmd, input=None):
@@ -25,8 +28,29 @@ class GitSSH(object):
 		env = {}
 		env.update( os.environ )
 		env.update({
-			'GIT_IDENTITY_FILE': self.privateFile
+			'GIT_IDENTITY_FILE': self.privateFile,
 			'GIT_SSH': git_ssh_cmd
 		})
-		proc = subprocess.Popen( params, env=env )
-		return proc.communicate( input=input )
+		self.proc = subprocess.Popen( params, cwd=self.repository, env=env )
+		out, err = self.proc.communicate( input=input )
+		code = self.proc.returncode
+		return out, err, code
+
+if __name__ == '__main__':
+	# Usage example
+
+	def get_key(name):
+		name = os.path.join( os.path.abspath( os.path.dirname(__file__) ), name )
+		with open(name) as f:
+			return f.read()
+
+	private = get_key('key')
+	public = get_key('key.pub')
+	folder = os.path.abspath( os.path.dirname(__file__) )
+	print 'Ready!', private, public, folder
+	with GitSSH(folder, private, public) as call:
+		print 'git pull ...'
+		out, err, code = call('git pull')
+		print "out: " + str(out)
+		print "err: " + str(err)
+		print "exit code: " + str(code)
