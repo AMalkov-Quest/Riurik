63d55a1c9f499bf8b201f1d8e79f49ce6269d541
Andrew Malkov <andrey.malkov@quest.com>
2012-10-09 08:57:39 +0400

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-sudo apt-get install python-setuptools tmux
-sudo easy_install django pygithub
+sudo apt-get install tmux
+sudo apt-get install python-setuptools
+sudo apt-get install -y python-pip
+sudo apt-get install -y nodejs npm
+
+sudo easy_install pygithub
+
+sudo npm install -g coffee-script
+
+sudo pip install Django
+sudo pip install Markdown
