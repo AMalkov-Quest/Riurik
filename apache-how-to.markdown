24ba1a857883ba81c85463ea774fd39e563b5879
Andrey Malkov <andrey.malkov@gmail.com>
2012-10-09 05:59:21 +0000

    documenting

---
+++ apache-how-to.markdown
@@ -0,0 +1,16 @@
+Installing Django with Apache on Ubuntu
+=======
+
+### Quick start
+Install Apache and mod_wsgi
+
+    sudo apt-get install apache2 libapache2-mod-wsgi
+
+Configure Apache
+
+    sudo vim /etc/apache2/httpd.conf
+	sudo vim ~/Riurik/riurik.wsgi
+
+Restart Apache
+
+    sudo /etc/init.d/apache2 restart
--- src/plugins/help/views.py
+++ src/plugins/help/views.py
@@ -4,19 +4,19 @@ import os.path
 import markdown
 import settings
 
-def __readme(request):
-	#readme_path = os.path.join(settings.root, 'README.markdown')
-	readme_path = os.path.join(os.path.dirname( __file__ ), 'README.markdown')
-	html = markdown.markdownFromFile(readme_path)
-	return HttpResponse(html)
-
 def readme(request):
-	md = markdown.Markdown()
-	readme_path = os.path.join(settings.root, 'README.markdown')
-	ifile = codecs.open(readme_path, mode="r", encoding="utf-8")
-	text = ifile.read()
-	ifile.close()
-	html = md.convert(text)
+	doc_name = request.REQUEST.get('doc', 'README.markdown')	
+	path = os.path.join(settings.root, doc_name)
+	html = markdown2html(path) 
 	return HttpResponse(html)
 
+def markdown2html(path):
+	text = markdown2text(path)
+	md = markdown.Markdown()
+	return md.convert(text)
 
+def markdown2text(path):
+	f = codecs.open(path, mode="r", encoding="utf-8")
+	text = f.read()
+	f.close()
+	return text
