6c6e28b6ea9f916915bfedea05072453e3896b54
Anton Timiskov <atimiskov@gmail.com>
2012-11-27 12:16:39 +0400

    Shortcuts and Saving

--- src/static/js/editor-connector.js
+++ src/static/js/editor-connector.js
@@ -3,4 +3,26 @@ $(function initAceEditor(){
     editor.setTheme("ace/theme/chrome");
     var mode = ( /\.coffee\?/.test(window.location.href) ) ? "coffee" : "javascript";
     editor.getSession().setMode(new (ace.require("ace/mode/"+mode).Mode)());
+
+    editor.commands.addCommand({
+        name: 'Save',
+        bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+        exec: function(editor) {
+            $('#save').trigger('click');
+        }
+    });
+
+    editor.commands.addCommand({
+        name: 'Run',
+        bindKey: {win: 'Ctrl-R',  mac: 'Command-R'},
+        exec: function(editor) {
+            $('#run').trigger('click');
+        }
+    });
+
+    editor.getSession().on('change', function(e) {
+        if ( mode == 'coffee' ) {
+            coffeeScriptSyntaxChecker(editor.getValue(), '#syntax');
+        }
+    });
 });
