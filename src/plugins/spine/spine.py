f33d86eca1f86d74b015638b9f8439d2fb17c148
Andrey Malkov <andrey.malkov@quest.com>
2012-12-05 10:27:10 +0400

    spine plugin

--- src/plugins/spine/spine.py
+++ src/plugins/spine/spine.py
@@ -6,30 +6,30 @@ def spine(ctx):
 	return ctx.get('spine', None) != None
 
 def wait_hem(host, port, timeout=10):
-    while True:
-        time.sleep(1)
-        try:
-            s = socket.create_connection( (host, port), 20)
-            return s.close()
-        except socket.error:
-            timeout = timeout -1
-            if not timeout:
-                raise Exception('hem server start timeout')
+	while True:
+		time.sleep(1)
+		try:
+			s = socket.create_connection( (host, port), 20)
+			return s.close()
+		except socket.error:
+			timeout = timeout -1
+			if not timeout:
+				raise Exception('hem server start timeout')
 
 def run_hem_server(ctx, path, fullpath, server):
-    global hem
-    if hem:
-        hem.terminate()
-        hem = None
+	global hem
+	if hem:
+		hem.terminate()
+		hem = None
 
-    port = ctx.get('port', 80)
-    host = ctx.get('host', server)
-    hem_path = os.path.join(fullpath, "../node_modules/hem/bin/hem")
-    hem = subprocess.Popen(
-        (hem_path, "server", "--port=%s" % port),
-        cwd = fullpath,
-        stdout = open( '/dev/null', 'w' ), stderr = subprocess.STDOUT
-    )
+		port = ctx.get('port', 80)
+		host = ctx.get('host', server)
+		hem_path = os.path.join(fullpath, "../node_modules/hem/bin/hem")
+		hem = subprocess.Popen(
+			(hem_path, "server", "--port=%s" % port),
+			cwd = fullpath,
+			stdout = open( '/dev/null', 'w' ), stderr = subprocess.STDOUT
+		)
 
-    wait_hem( host, port )
-    return "http://%s:%s/test" % (host, port)
+		wait_hem( host, port )
+		return "http://%s:%s/test" % (host, port)
