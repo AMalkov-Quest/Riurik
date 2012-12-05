0b6a91867f1c068d2ef97aa58303d22c051dc37b
Andrey Malkov <andrey.malkov@quest.com>
2012-12-05 09:25:06 +0400

    polish

---
+++ src/local_settings.py
@@ -0,0 +1,3 @@
+# this file is intended to hold you local settings those should not be commited
+# and pushed into the repository. To ignore it's modifications use next command
+# git update-index --assume-unchanged src/local_settings.py
