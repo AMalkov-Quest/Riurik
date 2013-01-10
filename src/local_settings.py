405d55fbcbc8ec09fd9defc1eedbc2ae7a2e454b
Andrey Malkov <riurik.framework@gmail.com>
2013-01-10 08:50:15 +0000

    polish

--- src/local_settings.py
+++ src/local_settings.py
@@ -4,18 +4,3 @@
 
 # uncomment this line to switch on the github plugin
 # use_github = True
-import os
-
-product_jscode_path = ('SharePoint Information Portal', 'Application', 'templates', 'static')
-riurik_tests_path = ('tests', 'riurik', 'cases')
-root = os.path.dirname( __file__ )
-
-VIRTUAL_PATHS = {
-  'info-portal-tests': ('c:\\hunter',) +  riurik_tests_path,
-  'warrior': ('c:\\warrior',) +  riurik_tests_path,
-  'wizard': ('c:\\wizard',) +  riurik_tests_path,
-  'blade': ('c:\\blade',) +  riurik_tests_path,
-  'caissa': ('c:\\caissa',) +  riurik_tests_path,
-  'hunter-dev': ('c:\\hunter-dev',) +  riurik_tests_path,
-  'riurik-inner-tests': (root, 'tests', 'cases'),
-}
