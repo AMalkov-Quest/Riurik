70ed2d30188168b34f36bccdbbf9e11ae67fc1cb
unknown <amalkov@spb8160.prod.quest.corp>
2013-09-12 12:00:55 +0400

    reconfigure

--- src/virtual_paths.py
+++ src/virtual_paths.py
@@ -3,12 +3,8 @@ import os, sys
 product_jscode_path = ('SharePoint Information Portal', 'Application', 'templates', 'static')
 riurik_tests_path = ('tests', 'riurik', 'cases')
 VIRTUAL_PATHS = {
-  'info-portal-tests': ('c:\\hunter',) +  riurik_tests_path,
-  'warrior': ('c:\\warrior',) +  riurik_tests_path,
-  'wizard': ('c:\\wizard',) +  riurik_tests_path,
-  'blade': ('c:\\blade',) +  riurik_tests_path,
-  'caissa': ('c:\\caissa',) +  riurik_tests_path,
-  'hunter-dev': ('c:\\hunter-dev',) +  riurik_tests_path,
+  'warrior': ('c:\\SASPWarriorBranches',) +  riurik_tests_path,
+  'wizard': ('c:\\SASPWizardBranch1',) +  riurik_tests_path,
 }
 
-sys.path.append('C:\\hunter\\tests')
+sys.path.append('C:\\SASPWizardBranch1\\tests')
