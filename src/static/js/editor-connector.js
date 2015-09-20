cdf45be63c4925ab7d26d0f8308989a9810467e3
Andrey.Malkov <Andrey.Malkov@software.dell.com>
2015-09-20 12:26:31 +0300

    daspec with ace

--- src/static/js/editor-connector.js
+++ src/static/js/editor-connector.js
@@ -11,7 +11,8 @@ $(function initAceEditor(){
     };
     var mode = modeprob('coffee', 'coffee')
             || modeprob('feature', 'gherkin')
-            || modeprob('js', 'javascript');
+            || modeprob('js', 'javascript')
+            || modeprob('daspec', 'markdown');
    
     console.log(mode) 
     if (mode != null) {
@@ -31,6 +32,17 @@ $(function initAceEditor(){
         bindKey: {win: 'Ctrl-R',  mac: 'Command-R'},
         exec: function(editor) {
             $('#run').trigger('click');
+        }
+    });
+	
+	editor.commands.addCommand({
+        name: 'Run',
+        bindKey: {win: 'Ctrl-M',  mac: 'Command-M'},
+        exec: function(editor) {
+            var converter = new showdown.Converter({simplifiedAutoLink: true, strikethrough: true, ghCodeBlocks: true, tables: true})
+			//window.alert( converter.makeHtml(editor.getValue()) );
+			var newWindow = window.open("", "newWindow", "resizable=yes");
+			newWindow.document.write(converter.makeHtml(editor.getValue()));
         }
     });
 
