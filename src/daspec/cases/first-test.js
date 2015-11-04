1343832edab65ee10ae23ac56da6c8bd9a6ad838
Andrey.Malkov <Andrey.Malkov@software.dell.com>
2015-11-04 21:28:30 +0300

    Hello world test

--- src/daspec/cases/first-test.js
+++ src/daspec/cases/first-test.js
@@ -1,33 +1,19 @@
-var SalaryCalculator = function () {
-    'use strict';
-	var bonusTable = {
-		'manager': { percent: 20, cap: 50000 },
-		'lab rat': { percent: 10, cap: 20000 }
-	};
-	this.bonusFor = function (role, salary) {
-		var bonusType = bonusTable[role.toLowerCase()];
-		if (!bonusType) {
-			return 0;
-		}
-		return Math.min(bonusType.cap, bonusType.percent * salary / 100);
-	};
+riurik.engine.steps_definition = function() {
+    
+function greetingFor (subject) {
+    return 'Hello, ' + subject + '!';
+}
+
+// this adds a function to process any line of text matching the regular expression
+// match groups in the regular expression will be passed to the function in the order from left to right
+//
+// So the sentence 'The right way to greet the World is "Hello, World!" will cause DaSpec to call
+// the function and pass World as the subject, and Hello, World! as the expected greeting.
+
+defineStep(/The right way to greet the (.*) is "(.*)"/, function (subject, expectedGreeting) {
+    var actualResult = greetingFor(subject);
+
+	expect(actualResult).toEqual(expectedGreeting);
+});
+
 };
-
-/*global defineStep, SalaryCalculator, expect*/
-
-// each line of markdown is evaluated against a regex - and the matching parts are passed to the function
-
-defineStep(/A (.*) with a (\d*) salary should get (\d*) as the end of year bonus/, function (role, salary, expectedBonus) {
-    'use strict';
-	var calculator = new SalaryCalculator();
-	// this expectation is related to an argument, so it will mark only the relevant
-	// part of the sentence as passed or failed
-	expect(calculator.bonusFor(role, salary)).toEqual(expectedBonus);
-});
-defineStep(/A (.*) with a (\d*) salary should get no bonus/, function (role, salary) {
-	'use strict';
-	var calculator = new SalaryCalculator();
-	// this will mark the entire sentence as passed or failed because
-	// it's not related to a particular argument
-	expect(calculator.bonusFor(role, salary)).toEqual(0);
-});
--- src/static/engines/daspec/connector.js
+++ src/static/engines/daspec/connector.js
@@ -9,6 +9,7 @@ riurik.engine.init = function( next ){
 	.queue('/static/engines/daspec/daspec-web.js')
 	.queue('/static/engines/daspec/daspec.html.js')
 	.then(function() {
+		$.extend(riurik.exports);
 		riurik.engine.config();
 		load_remote_style('/static/engines/daspec/daspec.css');
 		riurik.trigger( "riurik.engine.inited" );
@@ -16,9 +17,30 @@ riurik.engine.init = function( next ){
 	});
 };
 
+riurik.load_tests = function(){
+	console.log('load daspec test ' + riurik.args.path);
+	
+	riurik.engine.run_tests();
+};
+
 riurik.engine.run_tests = function() {
 	console.log('start daspec tests...');
-	//riurik.engine.jasmineEnv.execute();
+	$.ajax({ 
+		url: riurik.args.path, 
+		success: function(markdown){ 
+			
+			riurik.loader().queue(riurik.args.path.replace(/daspec$/, 'js')).then(function() {
+				var runner = new DaSpec.Runner(riurik.engine.steps_definition),
+					resultFormatter = new DaSpec.MarkdownResultFormatter(runner);
+			
+				runner.execute(markdown);
+				
+				$('#outputArea').html(resultFormatter.formattedResults()); 
+			});
+		}, 
+		dataType: 'text'
+	});
+	
 };
 
 riurik.engine.config = function() {
