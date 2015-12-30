8f7986656608f33f29d19df4861482eaa47aff64
Andrey.Malkov <Andrey.Malkov@software.dell.com>
2015-12-30 07:52:03 +0300

    show folder content

--- manage.py
+++ manage.py
@@ -8,7 +8,9 @@ import src.plugins.git
 from src.plugins.git import urls
 import src.plugins.github
 from src.plugins.github import urls
+from src.templatetags import dir_index_tags
 
+from django.core import context_processors
 from django.db.backends.dummy import base
 from django.core.cache.backends.locmem import LocMemCache
 from django.contrib.sessions import middleware, serializers
--- run-exe.bat
+++ run-exe.bat
@@ -1,3 +1,3 @@
-SET DJANGO_SETTINGS_MODULE=settings
+SET DJANGO_SETTINGS_MODULE=src.settings
 dist\riurik\riurik.exe runserver 0.0.0.0:83
 pause
