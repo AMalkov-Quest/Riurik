aeaf804e22de7eb685987efacc2569435fa2d2ab
Andrey.Malkov <Andrey.Malkov@software.dell.com>
2016-01-04 11:50:24 +0300

    install nodejs

--- IExpress/riurik-installer.ps1
+++ IExpress/riurik-installer.ps1
@@ -14,5 +14,23 @@ if( Test-Path $archivePath) {
 	}
 }
 
+function AreYouAgree() {
+    $title = "It is necessary to install Node.js"
+    $message = "Are you agree?"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Yes"
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "No"
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    $choice = $host.ui.PromptForChoice($title, $message, $options, 1)
+    return $choice
+}
+if( -not (Get-WmiObject -Class Win32_Product | where { $_.Name -match “Node.js”} ) ) {
+    $agree = AreYouAgree
+    Write-Host $agree
+    if( $agree -eq 0 ) {
+        iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+        iex 'choco install nodejs -y'
+    }
+}
+
 [Environment]::SetEnvironmentVariable("DJANGO_SETTINGS_MODULE", "src.settings", "Process")
 Start-Process $cwd\riurik\riurik.exe "runserver 0.0.0.0:8080" -PassThru
